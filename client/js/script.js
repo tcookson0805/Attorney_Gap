53bf843769606753248c5973a628591a9587f8d4
Tyler Cookson <tcookson0805@gmail.com>
2017-01-05 15:17:06 -0800

    styling modifications

--- client/js/script.js
+++ client/js/script.js
@@ -1,7 +1,8 @@
   
 $(document).ready(function() {
   
-  
+  $('.parallax').parallax();
+
   
   $('.chips').material_chip();
   $('.chips-initial').material_chip({
@@ -91,29 +92,54 @@ $(document).ready(function() {
           
           console.log(appearance);
           
+          var calendar = ['n/a', 'Jan', 'Feb', 'March', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+          
+          var courtName = appearance.courtInfo.courtName;
+          var place = appearance.courtInfo.courtAddress.city + ', ' + appearance.courtInfo.courtAddress.state;
+          var appearanceTime = appearance.appearanceTime;
+          var appearanceType = appearance.appearanceType;
+          
+          var year = appearance.appearanceDate.slice(0,4);
+          var monthNum = appearance.appearanceDate.slice(5,7);          
+          var day = appearance.appearanceDate.slice(8,10);
+          
+          var month = calendar[monthNum];
+          
+          var appearanceNew = 'new';
+          var appearanceDay = day + ' ' + month;
+          var appearanceYear = year;
+          
+          var caseType = appearance.caseType;
+          var caseHeader = appearance.caseHeader;
+          var caseNumber = appearance.caseNumber;
+          var clientType = appearance.clientInfo.clientType;
+          
+          var clientName = appearance.clientInfo.name;
+          var reqAttorney = appearance.reqAttorney;
+          var appearanceId = appearance._id;
           
           if(date) {
             
               if(appearance.courtInfo.courtAddress.state === state && appearance.courtInfo.courtAddress.county === county && appearance.appearanceDate === date) {
               
-              var appearanceTemp = $('.appearance_search_result_temp').clone()
+              var appearanceTemp = $('.search_result_temp').clone()
 
-              appearanceTemp.removeClass('appearance_search_result_temp');
-              appearanceTemp.addClass(('appearance_search_result'));
+              // appearanceTemp.removeClass('appearance_search_result_temp');
+              // appearanceTemp.addClass(('appearance_search_result'));
                                         
-              appearanceTemp.find('.appearance_search_result_appearanceType').text(appearance.appearanceType);
-              appearanceTemp.find('.appearance_search_result_caseType').text(appearance.caseType);
-              appearanceTemp.find('.appearance_search_result_caseHeader').text(appearance.caseHeader);
-              appearanceTemp.find('.appearance_search_result_caseNumber').text(appearance.caseNumber);
-              appearanceTemp.find('.appearance_search_result_appearanceDate').text(appearance.appearanceDate.slice(0,10));
-              appearanceTemp.find('.appearance_search_result_appearanceTime').text(appearance.appearanceTime);
-              appearanceTemp.find('.appearance_search_result_clientType').text(appearance.clientInfo.clientType);
-              appearanceTemp.find('.appearance_search_result_clientName').text(appearance.clientInfo.name);
-              appearanceTemp.find('.appearance_search_result_reqAttorney').text(appearance.reqAttorney);
-              appearanceTemp.find('.appearance_search_result_appearanceID').text(appearance._id);
+              // appearanceTemp.find('.appearance_search_result_appearanceType').text(appearance.appearanceType);
+              // appearanceTemp.find('.appearance_search_result_caseType').text(appearance.caseType);
+              // appearanceTemp.find('.appearance_search_result_caseHeader').text(appearance.caseHeader);
+              // appearanceTemp.find('.appearance_search_result_caseNumber').text(appearance.caseNumber);
+              // appearanceTemp.find('.appearance_search_result_appearanceDate').text(appearance.appearanceDate.slice(0,10));
+              // appearanceTemp.find('.appearance_search_result_appearanceTime').text(appearance.appearanceTime);
+              // appearanceTemp.find('.appearance_search_result_clientType').text(appearance.clientInfo.clientType);
+              // appearanceTemp.find('.appearance_search_result_clientName').text(appearance.clientInfo.name);
+              // appearanceTemp.find('.appearance_search_result_reqAttorney').text(appearance.reqAttorney);
+              // appearanceTemp.find('.appearance_search_result_appearanceID').text(appearance._id);
               
               
-              appearanceTemp.find('.appearance_search_result_accept').attr('href', '/api/appearances/' + appearance._id + '/accept');              
+              // appearanceTemp.find('.appearance_search_result_accept').attr('href', '/api/appearances/' + appearance._id + '/accept');              
               
               $('.appearance_search_results').append(appearanceTemp);
               $('.collapsible').collapsible();
@@ -124,24 +150,39 @@ $(document).ready(function() {
 
             if(appearance.courtInfo.courtAddress.state === state && appearance.courtInfo.courtAddress.county === county) {
               
-              var appearanceTemp = $('.appearance_search_result_temp').clone()
+              var appearanceTemp = $('.search_result_temp').clone()
+              appearanceTemp.removeClass('search_result_temp');
+              appearanceTemp.addClass(('search_result'));
 
-              appearanceTemp.removeClass('appearance_search_result_temp');
-              appearanceTemp.addClass(('appearance_search_result'));
+              appearanceTemp.find('.search_result_appearanceNew').text(appearanceNew);  
+              appearanceTemp.find('.search_result_appearanceDay').text(appearanceDay);
+              appearanceTemp.find('.search_result_appearanceYear').text(appearanceYear);
+                        
+              appearanceTemp.find('.search_result_courtName').text(courtName);
+              appearanceTemp.find('.search_result_place').text(place);
+              appearanceTemp.find('.search_result_appearanceTime').text(appearanceTime);                          
+              appearanceTemp.find('.search_result_appearanceType').text(appearanceType);
 
-                          
-              appearanceTemp.find('.appearance_search_result_appearanceType').text(appearance.appearanceType);
-              appearanceTemp.find('.appearance_search_result_caseType').text(appearance.caseType);
-              appearanceTemp.find('.appearance_search_result_caseHeader').text(appearance.caseHeader);
-              appearanceTemp.find('.appearance_search_result_caseNumber').text(appearance.caseNumber);
-              appearanceTemp.find('.appearance_search_result_appearanceDate').text(appearance.appearanceDate.slice(0,10));
-              appearanceTemp.find('.appearance_search_result_appearanceTime').text(appearance.appearanceTime);
-              appearanceTemp.find('.appearance_search_result_clientType').text(appearance.clientInfo.clientType);
-              appearanceTemp.find('.appearance_search_result_clientName').text(appearance.clientInfo.name);
-              appearanceTemp.find('.appearance_search_result_reqAttorney').text(appearance.reqAttorney);
-              appearanceTemp.find('.appearance_search_result_appearanceID').text(appearance._id);
-              appearanceTemp.find('.appearance_search_result_instruction').text(appearance.instructions);
-              appearanceTemp.find('.appearance_search_result_accept').attr('href', '/api/appearances/' + appearance._id + '/accept');              
+              appearanceTemp.find('.search_result_caseType').text(caseType);
+              appearanceTemp.find('.search_result_caseHeader').text(caseHeader);
+              appearanceTemp.find('.search_result_caseNumber').text(caseNumber);
+              appearanceTemp.find('.search_result_clientType').text(clientType);
+
+              appearanceTemp.find('.search_result_clientName').text(clientName);
+              appearanceTemp.find('.search_result_reqAttorney').text(reqAttorney);
+              appearanceTemp.find('.search_result_appearanceID').text(appearanceId);
+              
+              // appearanceTemp.find('.appearance_search_result_caseType').text(appearance.caseType);
+              // appearanceTemp.find('.appearance_search_result_caseHeader').text(appearance.caseHeader);
+              // appearanceTemp.find('.appearance_search_result_caseNumber').text(appearance.caseNumber);
+              // appearanceTemp.find('.appearance_search_result_appearanceDate').text(appearance.appearanceDate.slice(0,10));
+              // appearanceTemp.find('.appearance_search_result_appearanceTime').text(appearance.appearanceTime);
+              // appearanceTemp.find('.appearance_search_result_clientType').text(appearance.clientInfo.clientType);
+              // appearanceTemp.find('.appearance_search_result_clientName').text(appearance.clientInfo.name);
+              // appearanceTemp.find('.appearance_search_result_reqAttorney').text(appearance.reqAttorney);
+              // appearanceTemp.find('.appearance_search_result_appearanceID').text(appearance._id);
+              // appearanceTemp.find('.appearance_search_result_instruction').text(appearance.instructions);
+              // appearanceTemp.find('.appearance_search_result_accept').attr('href', '/api/appearances/' + appearance._id + '/accept');              
 
               
               $('.appearance_search_results').append(appearanceTemp);
@@ -175,6 +216,7 @@ $(document).ready(function() {
     })
     
   });
+  
 
 });
   
